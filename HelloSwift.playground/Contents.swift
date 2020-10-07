209e953a0c99992a9c55d2982df657cbe5e018c2
hanbee <sonhanbi1002@gmail.com>
2020-10-07 23:09:39 +0900

    init commit

---
+++ HelloSwift.playground/Contents.swift
@@ -0,0 +1,20 @@
+// swift 주석
+// sigle line 주석
+/*
+ multi
+ line
+ 주석
+ */
+var /* inline 주석 */ num = 0
+
+// framework, library 를 임포트 하는 부분
+// framework: 특정 기능을 구현한 코드를 하나의 이름으로 포장한 것
+import UIKit
+import MapKit // 지도 기능 사용 framework
+
+// str 변수
+var str = "Hello, Swift"
+print(str) // print 함수 호출
+dump(str)  // 더 자세한 출력을 할 수 있는 함수
+
+// swift 는 대소문자를 확실하게 구분함!
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+// playground 사용법 익히기
+import UIKit
+
+var str = "Hello, swift"
+print(str)
+
+let r = CGRect(x: 0, y: 10, width: 20, height: 30)
+
+var sum = 0
+for i in 1...10 {
+    sum += i
+}
+print(sum)
